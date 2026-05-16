dd1dd4e5b6ed3fc59be6fb2ddca22a3d207a8396
sidni89 <sidniilma89@gmail.com>
2026-05-16 14:01:32 +0700

    security: hardening repository by removing local configs and private notes

--- get_sha1.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Program Files\Android\Android Studio\jbr\bin\keytool.exe" -list -v -keystore "C:\Users\izidn\.android\debug.keystore" -alias androiddebugkey -storepass android -keypass android
-pause
